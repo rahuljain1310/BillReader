f1092bb1e3c19f28046bc1b20eade37ed80c6507
Vishwajeet Agrawal <vishwa.grawal@gmail.com>
2020-06-08 23:13:00 +0530

    kb debugged

--- extract_knowledge.py
+++ extract_knowledge.py
@@ -7,13 +7,13 @@ def isNumber(s):
 	# return if text is a number valued, cannot span multiple lines
 	pass
 
-# list(list(list(dict(['text':string,'point':dict('minx','miny','maxx','maxy'),'details':)))
+# list(list(list(dict(['text':string,'point':dict('minX','minY','maxX','maxY'),'details':)))
 
-def is_align(p1,p2):
-	pass
+# def is_align(p1,p2):
+# 	pass
 def get_datav(p1,sect):
-	width = p1['maxy'] - p1['miny']
-	length = p1['maxx'] - p1['minx']
+	width = p1['maxY'] - p1['minY']
+	length = p1['maxX'] - p1['minX']
 	scores = np.zeros((len(sect)))
 	L = 0.1
 	R = 1.8
@@ -21,25 +21,27 @@ def get_datav(p1,sect):
 	SCORE_MIN = 1
 	for i  in range(len(sect)):
 		p = sect[i]['point']
-		w = p['maxy'] - p['miny']
-		l = p['maxx'] - p['minx']
-		xdiff = p1['minx'] - p['minx']
-		ydiff = p1['miny'] - p['maxy']
-		cx = min(p1['maxx'] - p['minx'], p['maxx'] - p1['minx'])
+		w = p['maxY'] - p['minY']
+		l = p['maxX'] - p['minX']
+		xdiff = p1['minX'] - p['minX'] + 0.01
+		ydiff = p1['minY'] - p['maxY'] + 0.01
+		cx = min(p1['maxX'] - p['minX'], p['maxX'] - p1['minX'])
 
 		if (w/width > R or width/w > R):
 			scores[i] = 0
 		# elif (ydiff/np.sqrt(w*width) < 0.7):
-		ys = np.sqrt(w*width)/ydiff 
-		if (ys < 1):
-			scores[i] = 0
+		ys = ydiff/np.sqrt(w*width)
 		xs1 = np.sqrt(l*length)/xdiff
 		xs2 = cx/np.sqrt(l*length)
+		scores[i] = ys + xs1 + 2*xs2
+		
+		if (ys > 1.3):
+			scores[i] = 0
 		if (xs2 < 0):
 			scores[i] = 0
 		if (xs1 < 2):
 			scores[i] = 0
-		scores[i] = ys + xs1 + 2*xs2
+		
 
 	ju = np.argmax(scores)
 	if (scores[ju] < SCORE_MIN):
@@ -47,11 +49,14 @@ def get_datav(p1,sect):
 	return ju
 
 def is_align(p1,p2):
-	u_diff = p2['miny'] - p1['miny']
-	b_diff = p2['maxy'] - p1['maxy']
+	u_diff = p2['minY'] - p1['minY']
+	b_diff = p2['maxY'] - p1['maxY']
+	w1 = p1['maxY'] - p1['minY']
+	w2 = p2['maxY'] - p1['minY']
+	w = np.sqrt(w1*w2)
 	w_diff = abs(u_diff - b_diff)
-	L = 0.1
-	if (abs(u_diff) > L  or abs(b_diff) > L or w_diff > L):
+	L = 2
+	if (abs(u_diff/w) > L  or abs(b_diff/w) > L or w_diff/w > L):
 		return False
 	else:
 		return True
@@ -71,6 +76,7 @@ def group(section):
 		dl = section[i]	
 		# if 
 		for j in range(len(section[i])):
+			section[i][j]['matched'] = False
 			if dl[j]['details']['isNumber']:
 		 		smatched[i][j] = True
 			ginc = False
@@ -79,14 +85,18 @@ def group(section):
 					if (j==0):
 						ginc = True
 					elif ( is_align(dl[j]['point'],dl[j-1]['point'])):
+						# previoues keyword and unmatched
 						if (dl[j-1]['details']['isKeyword'] and (not (matched[i][j-1]))):
-							matched[i][j] = True
-							section[i][j]['matched'] = True
+							matched[i][j-1] = True
+							section[i][j-1]['matched'] = True
 							grouped[i][j] = grouped[i][j-1]
+						# previous keyword matched or value matched
 						elif (dl[j-1]['details']['isKeyword'] or matched[i][j-1]):
 							ginc = True
 						else:
+							# previous value unmatched
 							grouped[i][j] = grouped[i][j-1]
+							
 					else:
 						ginc = True
 					# if (dl[j]['details']['isNumber']):
@@ -106,7 +116,10 @@ def group(section):
 								# 	matched[i][j] = True
 								grouped[i][j] = grouped[i][j-1] ## aligned with left
 							elif (dl[j-1]['details']['isKeyword'] or matched[i][j-1]):
-								check_above = True
+								if (dl[j]['isNumber']):
+									ginc=True
+								else:
+									check_above = True
 							else:
 								grouped[i][j] = grouped[i][j-1] ## aligned with left
 	
@@ -119,9 +132,13 @@ def group(section):
 						if (ju==-1 or  matched[i-1][ju] or smatched[i-1][ju]):
 							ginc = True
 						else:
+							## above is key unmatched
 							if (section[i-1][ju]['details']['isKeyword']):
 								matched[i-1][ju] = True
 								section[i-1][ju]['matched'] = True
+								grouped[i][j] = grouped[i-1][ju]
+							## above is value unmatched
+							else:
 								grouped[i][j] = grouped[i-1][ju]
 								# if (dl[j]['details']['isNumber']):
 								# 	matched[i][j] = True
@@ -150,23 +167,19 @@ def extract(data):
 		groups.extend(group(section))
 	i = 0
 	l = len(groups)
-	extracted =[False]*len(l)
+	extracted =[False]*l
 	for g in groups:
 		if (len(g)==0):
 			extracted[i] = True
-		
 		elif (len(g)==1):
 			knwoledge[str(i)+':'] = g[0]
 			extracted[i] = True
-
-		elif (g[0]['details']['isKeyword'] and 'matched' in g[0]):
+		elif (g[0]['details']['isKeyword'] and g[0]['matched']):
 			pkey = g[0]
-			value =  ' '.join(g[1:])
-			knwoledge[str(i) + ':' + pkey] = value
+			value =  g[1:]
+			knwoledge[str(i) + ':' + pkey['text']] = value
 			extracted[i] = True
-		
 		i+=1
-
 	## recognize table
 	def half_keys(g):
 		numk = 0
@@ -177,7 +190,7 @@ def extract(data):
 			return True
 		else:
 			return False
-	return knwoledge
+	return groups,knwoledge
 	# i = 0
 	# while(i<len(groups)):
 	# 	if not extracted[i]:
